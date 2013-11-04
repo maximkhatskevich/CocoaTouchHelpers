1e163e8c969f69c6eb9fe39398ecafad0f6fb725
Maxim Khatskevich <maxim.khatskevich@gmail.com>
2013-11-04 14:46:46 +0600

    Initial Xcode project files have been added.

---
+++ Src/MyHelpers.h
@@ -0,0 +1,24 @@
+//
+//  MyHelpers.h
+//  MyHelpers
+//
+//  Created by Maxim Khatskevich on 11/4/13.
+//  Copyright (c) 2013 Maxim Khatskevich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//=== global level headers
+
+//#import <MyHelpers/MacrosBase.h>
+//#import <MyHelpers/GlobalBase.h>
+
+//=== classes
+
+// ...
+
+//=== categories
+
+// ...
+
+@end

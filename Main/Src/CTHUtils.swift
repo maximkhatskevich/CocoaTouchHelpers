c068839101c448433d8ba85b2eb12e1813e71c68
maximkhatskevich <maxim.khatskevich@gmail.com>
2014-11-14 21:08:32 +0000

    Swift experiment

---
+++ Main/Src/CTHUtils.swift
@@ -0,0 +1,14 @@
+//
+//  CTHUtils.swift
+//  CocoaTouchHelpers
+//
+//  Created by Maxim Khatskevich on 14/11/14.
+//  Copyright (c) 2014 Maxim Khatskevich. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//===
+
+func mainScreenSize() -> CGSize { return UIScreen.mainScreen().bounds.size }
